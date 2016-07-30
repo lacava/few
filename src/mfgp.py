f3c769f58f4ce1b87e87224c22c5680b7e76235d
William La Cava <williamlacava@gmail.com>
2016-07-30 14:04:31 -0400

    first commit

---
+++ src/mfgp.py
@@ -0,0 +1,11 @@
+# William La Cava
+# 2016 - 07 - 29
+# multidimensional feature genetic programming (aka motha fuckin GP!)
+#
+
+print "hello"
+
+def frunction():
+    print "hello from frunction"
+
+frunction()
