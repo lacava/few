fe3ec6f70a5de73291d44b55c3a55b55e228d25d
lacava <williamlacava@gmail.com>
2016-09-14 16:02:23 -0400

    updates comments

--- few/__init__.py
+++ few/__init__.py
@@ -1,21 +1,12 @@
 # -*- coding: utf-8 -*-
 
-'''
-Copyright 2016 Randal S. Olson
+"""
+Copyright 2016 William La Cava
 
-This file is part of the TPOT library.
+This file is part of the FEW library.
 
-The TPOT library is free software: you can redistribute it and/or
-modify it under the terms of the GNU General Public License as published by the
-Free Software Foundation, either version 3 of the License, or (at your option)
-any later version.
-
-The TPOT library is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with
-the TPOT library. If not, see http://www.gnu.org/licenses/.
-'''
+License: GPLv3
+"""
 
 from ._version import __version__
 from .few import FEW, main
