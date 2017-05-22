8363d490e9ba9c6b7207415ac15ef8bad81fce6e
lacava <williamlacava@gmail.com>
2017-05-22 11:47:23 -0400

    comments out WIP on few_lib.pyx

--- few/lib/few_lib.pyx
+++ few/lib/few_lib.pyx
@@ -12,14 +12,14 @@ cdef extern from "epsilon_lexicase.h":
 def ep_lex(np.ndarray F, int n, int d, int num_selections, np.ndarray locs):
     return _epsilon_lexicase(Map[ArrayXXd](F), n, d, num_selections,
                                Map[ArrayXi](locs))
+# WIP
+# cdef extern from "evaluation.h":
+#     cdef void _evaluate "evaluate"(node n, Map[ArrayXXd] & features,
+#                                    vector[Map[ArrayXd]]] stack_float,
+#                                    vector[Map[ArrayXb]] stack_bool)
 
-cdef extern from "evaluation.h":
-    cdef void _evaluate "evaluate"(node n, Map[ArrayXXd] & features,
-                                   vector[Map[ArrayXd]]] stack_float,
-                                   vector[Map[ArrayXb]] stack_bool)
-
-def evaluate(node n, np.ndarray features, vector[np.ndarray] stack_float,
-             vector[np.ndarray] stack_bool):
-    return _evaluate(node n, Map[ArrayXXd](features),
-                     vector[Map[ArrayXd]]](stack_float),
-                     vector[Map[ArrayXb]](stack_bool))
+# def evaluate(node n, np.ndarray features, vector[np.ndarray] stack_float,
+#              vector[np.ndarray] stack_bool):
+#     return _evaluate(node n, Map[ArrayXXd](features),
+#                      vector[Map[ArrayXd]]](stack_float),
+#                      vector[Map[ArrayXb]](stack_bool))
