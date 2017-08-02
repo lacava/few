be1b7e95993ac51f8c550331f6437cd21e736a07
lacava <williamlacava@gmail.com>
2017-08-02 15:37:57 -0400

    shortens line widths, updates version

--- few/lib/few_lib.pyx
+++ few/lib/few_lib.pyx
@@ -7,20 +7,11 @@ from libcpp cimport bool
 cdef extern from "epsilon_lexicase.h":
      cdef void _epsilon_lexicase "epsilon_lexicase"(Map[ArrayXXd] & F, int n,
                                                     int d, int num_selections,
-                                                    Map[ArrayXi] & locs, bool lex_size, Map[ArrayXi] &sizes)
+                                                    Map[ArrayXi] & locs, bool lex_size,
+                                                    Map[ArrayXi] &sizes)
 
 # This will be exposed to Python
-def ep_lex(np.ndarray F, int n, int d, int num_selections, np.ndarray locs, bool lex_size, np.ndarray sizes):
+def ep_lex(np.ndarray F, int n, int d, int num_selections, np.ndarray locs, bool lex_size,
+           np.ndarray sizes):
     return _epsilon_lexicase(Map[ArrayXXd](F), n, d, num_selections,
                                Map[ArrayXi](locs), lex_size, Map[ArrayXi](sizes))
-# WIP
-# cdef extern from "evaluation.h":
-#     cdef void _evaluate "evaluate"(node n, Map[ArrayXXd] & features,
-#                                    vector[Map[ArrayXd]]] stack_float,
-#                                    vector[Map[ArrayXb]] stack_bool)
-
-# def evaluate(node n, np.ndarray features, vector[np.ndarray] stack_float,
-#              vector[np.ndarray] stack_bool):
-#     return _evaluate(node n, Map[ArrayXXd](features),
-#                      vector[Map[ArrayXd]]](stack_float),
-#                      vector[Map[ArrayXb]](stack_bool))
